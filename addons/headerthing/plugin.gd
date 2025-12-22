fea0a0e82b4600dac7d8dde64b8d47b239ccbe0a
Aren <aren.permadi@digipen.edu>
2025-12-22 00:53:32 -0800

    getting started on the plugin - added variables and print lines if objects are null - progress...?

--- addons/headerthing/plugin.gd
+++ addons/headerthing/plugin.gd
@@ -1,6 +1,9 @@
 @tool
 extends EditorPlugin
 
+var script_editor: ScriptEditor
+var current_script : Script
+var code
 
 func _enable_plugin() -> void:
 	# Add autoloads here.
@@ -13,10 +16,39 @@ func _disable_plugin() -> void:
 
 
 func _enter_tree() -> void:
-	# Initialization of the plugin goes here.
+	script_editor = EditorInterface.get_script_editor()	
+	
+	if (script_editor == null): 
+		print("headerthing says: script editor is null")
+		pass
+	
+	current_script = script_editor.get_script()
+	if (current_script == null): 
+		print("headerthing says: current script is null")
+		pass
+	
+	# _print_all_children(script_editor)
 	pass
 
 
+func _process(delta: float) -> void:
+	
+	pass
+
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
+	pass
+
+
+# debugging purposes
+func _print_all_children(node: Node) -> void:
+	for child in node.get_children():
+		if (child.get_child_count()):
+			_print_all_children(child)
+		
+		# print(child.name)
+
+
+func _parse_source_code(code : String) -> void:
+	
 	pass
