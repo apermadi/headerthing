046e5e10565b012b32e3b52835815517ed3307b9
Aren <aren.permadi@digipen.edu>
2025-12-30 00:39:33 -0800

    intended behavior for the header is working
    - the comment to check for the header line is based on the header_source
    - still need to reload manually and tab out/tab in to add the header
    - BUG: randomly crashes, unable to replicate at this moment

--- addons/headerthing/plugin.gd
+++ addons/headerthing/plugin.gd
@@ -3,15 +3,15 @@
 # brief:
 # date:
 # ***
-
-# ***
 @tool
 extends EditorPlugin
 
 var file : FileAccess
-var header_source : PackedStringArray
 var script_editor: ScriptEditor
+var header_source : PackedStringArray
 var source_code : String
+var comment_line : String
+var performed : bool = false
 
 # debug printing
 var printed_once: bool = false
@@ -23,12 +23,11 @@ func _enable_plugin() -> void:
 
 func _disable_plugin() -> void:
 	# Remove autoloads here.
-	pass
+	pass 
 
 
 func _enter_tree() -> void:
 	# TODO: error checking
-	# this will not be a todo soon
 	file = FileAccess.open("res://addons/headerthing/header_source.txt", FileAccess.READ)
 	
 	header_source = file.get_as_text().split("\n")
@@ -41,8 +40,6 @@ func _enter_tree() -> void:
 	file.close()
 	script_editor.editor_script_changed.connect(_check_script)
 	
-	print(header_source)
-	
 	pass
 
 
@@ -50,13 +47,9 @@ func _process(delta: float) -> void:
 	var script := get_editor_interface().get_script_editor().get_current_script() 
 
 	if (!printed_once):
-		# print(script.get_source_code())
 		if script && script.has_source_code():
-			# print(script.source_code.get_slice("\n", 0))
-			
-			# TODO: replace "# *" with the first line of the source, up to two chars
-			# thats what we call data driven development !!
-			if (script.source_code.get_slice("\n", 0) == "# *"):
+			comment_line = header_source[0].substr(0, 3)
+			if (script.source_code.get_slice("\n", 0) == comment_line):
 				var new_code = script.source_code.split("\n")
 				new_code = _insert_header(new_code)
 				new_code = "\n".join(new_code)
@@ -65,8 +58,7 @@ func _process(delta: float) -> void:
 				if (ResourceSaver.save(script) != 0):
 					printerr("headerthing says: unable to save script")
 					pass
-			
-			print(script.source_code)
+				
 			printed_once = true
 	
 	pass
@@ -84,7 +76,7 @@ func _check_script(script: Script) -> void:
 
 func _insert_header(code: PackedStringArray) -> PackedStringArray:
 	code[0] = header_source[0]
-	for line in header_source.size():
+	for line in range(1, header_source.size()-1):
 		code.insert(line, header_source[line])
 		pass
 	
