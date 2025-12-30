87a1f253d0ec870e26d937619e2d00ed3c996ddc
Aren <aren.permadi@digipen.edu>
2025-12-29 18:57:57 -0800

    actually able to modify the file now
    - does NOT work as it should right now, as it replaces the first char of the string with 'Q'
    - have to tab out and tab back in to prompt the engine to reload

--- addons/headerthing/plugin.gd
+++ addons/headerthing/plugin.gd
@@ -1,9 +1,12 @@
 @tool
 extends EditorPlugin
 
-var script_editor: ScriptEditor
-var current_script : Script
-var code
+var script_editor: ScriptEditor = null
+var current_script = null
+var source_code : String
+
+# debug printing
+var printed_once: bool = false
 
 func _enable_plugin() -> void:
 	# Add autoloads here.
@@ -19,36 +22,50 @@ func _enter_tree() -> void:
 	script_editor = EditorInterface.get_script_editor()	
 	
 	if (script_editor == null): 
-		print("headerthing says: script editor is null")
+		printerr("headerthing says: script editor is null")
 		pass
 	
-	current_script = script_editor.get_script()
-	if (current_script == null): 
-		print("headerthing says: current script is null")
-		pass
+	script_editor.editor_script_changed.connect(_check_script)
 	
-	# _print_all_children(script_editor)
 	pass
 
 
+# TODO: every time the script has changed,
+# check in the first line of the function the string "#-"
+# if it is there, insert header into the source code
 func _process(delta: float) -> void:
+	# var script = EditorInterface.get_script_editor()
+	current_script = script_editor.get_open_scripts()
+	# var scripts = script_editor.get_open_scripts()
 	
+	var script := get_editor_interface().get_script_editor().get_current_script() 
+	# var source := script.get_source_code()
+	if (!printed_once):
+		# print(script.get_source_code())
+		if script && script.has_source_code():
+			print(script.source_code.get_slice("\n", 0))
+			
+			# script.source_code.insert(0, "# hello world!")
+			script.source_code[0] = 'Q'
+			ResourceSaver.save(script)
+			
+			print(script.source_code.get_slice("\n", 0))
+			
+			printed_once = true
+		
+		
+		
+		# print(current_script.get_source_code())
 	pass
+	
+	# script.reload()
+
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
 	pass
 
 
-# debugging purposes
-func _print_all_children(node: Node) -> void:
-	for child in node.get_children():
-		if (child.get_child_count()):
-			_print_all_children(child)
-		
-		# print(child.name)
-
-
-func _parse_source_code(code : String) -> void:
-	
+func _check_script(script: Script) -> void:
+	# print("beep")
 	pass
